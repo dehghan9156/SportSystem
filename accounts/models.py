7b28ca71c4b31d23bd01628a58c732665246b689
dehghan9156 <dehghan9156@gmail.com>
2025-07-20 09:02:22 +0330

    Add model User

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
+        if not username:
+            raise ValueError(_("The Username must be set"))
+        # email = self.normalize_email(email)
+        user = self.model(username=username,**extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(username, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    select_role = [
+        ("admin","Admin"),
+        ("race_manager","Race_Manager"),
+        ("dovar","Dovar"),
+        ("racer","Racer")
+    ]
+    name = models.CharField(max_length=250)
+    family =models.CharField(max_length=250)
+    username = models.CharField(max_length=250,unique=True)
+    role = models.CharField(choices=select_role,max_length=20)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+ 
