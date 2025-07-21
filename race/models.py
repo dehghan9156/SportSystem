dbb68db11117df67d872507fa0de5ece9fd9e8fe
dehghan9156 <dehghan9156@gmail.com>
2025-07-21 12:32:03 +0330

    Add model race,category

--- race/models.py
+++ race/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Race(models.Model):
+    title = models.CharField(max_length=250)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    longitude = models.DecimalField(max_digits=6, decimal_places=2)
+    latitude = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title}-{self.category}"
