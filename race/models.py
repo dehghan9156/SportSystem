83c286e1fcb1344bbe9477a26f14b9c10cefe646
dehghan9156 <dehghan9156@gmail.com>
2025-07-22 08:37:25 +0330

    Edit model race

--- race/models.py
+++ race/models.py
@@ -10,8 +10,8 @@ class Race(models.Model):
     title = models.CharField(max_length=250)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
-    longitude = models.DecimalField(max_digits=6, decimal_places=2)
-    latitude = models.DecimalField(max_digits=6, decimal_places=2)
+    longitude = models.DecimalField(max_digits=6, decimal_places=2,blank=True,null=True)
+    latitude = models.DecimalField(max_digits=6, decimal_places=2,blank=True,null=True)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
 
     def __str__(self):
